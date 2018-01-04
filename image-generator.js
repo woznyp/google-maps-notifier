e851141a3cfdec88a4eeb775a8bd87f302b3db2a
Paweł Woźny <pawel.wozny@pearson.com>
2018-01-04 11:24:02 +0100

    changing from sync to event listener

--- image-generator.js
+++ image-generator.js
@@ -5,24 +5,14 @@ args = system.args,
 filename = args[1];
 
 page.viewportSize = { width: 768, height: 1024 };
+page.onConsoleMessage = function(msg) {
+    if(msg === 'maploaded'){
+        const timeEnd = new Date().getTime();
 
-function finish(){
-    const window = page.evaluate(function(){
-        return window;
-    });
-    
-    const timeEnd = new Date().getTime();
-
-    if(window.imageGenerated){
         page.render('./' + filename + '.jpg');
         console.log('map loaded in:' + (timeEnd - timeBegin) + 'ms');
         phantom.exit();
-    } else {
-        console.log('waiting for map to be loaded; elapsed time:' + (timeEnd - timeBegin) + 'ms');
-        setTimeout(finish, 250);
     }
-}
+};
 
-page.open('./map-raw.html', function(){
-    finish();
-});
+page.open('./map-raw.html');
