1749a88d830fae78a024bc47516503c2de4bb1f3
Paweł Woźny <pawelpwozny@gmail.com>
2018-01-04 20:13:43 +0100

    adding delay for page render in phantomjs script

--- image-generator.js
+++ image-generator.js
@@ -7,11 +7,12 @@ filename = args[1];
 page.viewportSize = { width: 768, height: 1024 };
 page.onConsoleMessage = function(msg) {
     if(msg === 'maploaded'){
-        const timeEnd = new Date().getTime();
-
-        page.render('./' + filename + '.jpg');
-        console.log('map loaded in:' + (timeEnd - timeBegin) + 'ms');
-        phantom.exit();
+        setTimeout(function(){
+            const timeEnd = new Date().getTime();
+            page.render('./' + filename + '.jpg');
+            console.log('map loaded in:' + (timeEnd - timeBegin - 500) + 'ms');
+            phantom.exit();
+        }, 500);
     }
 };
 
